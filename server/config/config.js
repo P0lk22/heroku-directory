4be981d0efd19c74ab3c4b5cd7b96bc282efa970
Steven Polk <shpjr@earthlink.net>
2019-02-11 19:23:14 -0500

    credentials

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,5 @@ module.exports = {
   db: {
     uri: 'mongodb://spolk:shpjr2502@ds159993.mlab.com:59993/bc3', //place the URI of your mongo database here.
   }, 
-  port: 8080
+  port: process.env.PORT
 };
